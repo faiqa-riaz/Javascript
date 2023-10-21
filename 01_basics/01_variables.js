49688d21442fefb7aaa84b42406bfa994592a4bd
Faiqa Riaz <114059299+faiqa-riaz@users.noreply.github.com>
2023-10-21 18:07:21 +0000

    Variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 99100
+let accountEmail = "faiqariaz@hotmail.com"
+var accountPassword = "898910"
+accountCity = "Abbottabad"
+let accountState;
+
+accountId = 2234
+
+console.log(accountEmail);
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
